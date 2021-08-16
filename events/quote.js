f35428693950bf62b0e0235b382ff9b886733d81
chairielazizi <azizihr2000@gmail.com>
2021-08-16 09:58:40 +0800

    repair quote fx

--- events/quote.js
+++ events/quote.js
@@ -3,22 +3,26 @@ const client = new Discord.Client();
 const fetch = require("node-fetch");
 
 module.exports = (client) => {
-  // start to fetch the api from zenquote
-  const url = "https://zenquotes.io/api/random";
-  async function get(url) {
-    // Storing response
-    const response = await fetch(url);
+  client.guilds.cache.forEach((guild) => {
+    if (guild == process.env.guilds) {
+      // start to fetch the api from zenquote
+      const url = "https://zenquotes.io/api/random";
+      async function get(url) {
+        // Storing response
+        const response = await fetch(url);
 
-    // Storing data in form of JSON
-    var data = await response.json();
-    // console.log(data);
-    var output = data[0].q + " -" + data[0].a + "-";
-    console.log(output);
-    return output;
-  }
-  setInterval(async () => {
-    var channel = client.channels.cache.get("806020201682042921");
-    quote = await get(url);
-    channel.send(quote);
-  }, "28800000");
+        // Storing data in form of JSON
+        var data = await response.json();
+        // console.log(data);
+        var output = data[0].q + " -" + data[0].a + "-";
+        console.log(output);
+        return output;
+      }
+      setInterval(async () => {
+        var channel = client.channels.cache.get("806020201682042921");
+        quote = await get(url);
+        channel.send(quote);
+      }, "28800000");
+    }
+  });
 };
